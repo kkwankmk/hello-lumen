82441e9e727602fae83a9b3eb705d747c3ae4ddc
Khwanrudee Khumongkhol <khwanrudee@carro.co>
2018-10-02 00:12:57 +0700

    Chapter 6 responding to errors

--- tests/app/Http/Controllers/BooksControllerTest.php
+++ tests/app/Http/Controllers/BooksControllerTest.php
@@ -3,9 +3,11 @@
 namespace Tests\App\Http\Controllers;
 
 use TestCase;
+use Laravel\Lumen\Testing\DatabaseMigrations;
 
 class BooksControllerTest extends TestCase
 {
+    use DatabaseMigrations;
     /** @test **/
     public function index_status_code_should_be_200()
     {
@@ -15,20 +17,23 @@ class BooksControllerTest extends TestCase
     /** @test **/
     public function index_should_return_a_collection_of_records()
     {
-        $this->get('/books')
-            ->seeJson(['title' => 'War of the Worlds'])
-            ->seeJson(['title' => 'A Wrinkle in Time']);
+        $books = factory('App\Book', 2)->create();
+        $this->get('/books');
+        foreach ($books as $book) {
+            $this->seeJson(['title' => $book->title]);
+        }
     }
 
     /** @test **/
     public function show_should_return_a_valid_book()
     {
-        $this->get('/books/1')->seeStatusCode(200)
+        $books = factory('App\Book')->create();
+        $this->get("/books/{$books->id}")->seeStatusCode(200)
             ->seeJson([
-                'id' => 1,
-                'title' => 'War of the Worlds',
-                'description' => 'A science fiction masterpiece about Martians invading London',
-                'author' => 'H. G. Wells'
+                'id' => $books->id,
+                'title' => $books->title,
+                'description' => $books->description,
+                'author' => $books->author
             ]);
 
         $data = json_decode($this->response->getContent(), true);
@@ -39,11 +44,12 @@ class BooksControllerTest extends TestCase
     /** @test **/
     public function show_should_fail_when_the_book_id_does_not_exist()
     {
-        $this->get('/books/99999')->seeStatusCode(404)
+        $this
+            ->get('/books/99999', ['Accept' => 'application/json'])
+            ->seeStatusCode(404)
             ->seeJson([
-                'error' => [
-                    'message' => 'Book not found'
-                ]
+                'message' => 'Not Found',
+                'status' => 404
             ]);
     }
 
@@ -88,12 +94,14 @@ class BooksControllerTest extends TestCase
     /** @test **/
     public function update_should_only_change_fillable_fields()
     {
-        $this->notSeeInDatabase('books', [
-            'title' => 'The War of the Worlds'
+        $book = factory('App\Book')->create([
+            'title' => 'War of the Worlds',
+            'description' => 'A science fiction masterpiece about Martians invading London',
+            'author' => 'H. G. Wells'
         ]);
-
-        $this->put('/books/1', [
-            // 'id' => 5,
+       
+        $this->put("/books/{$book->id}", [
+            'id' => 5,
             'title' => 'The War of the Worlds',
             'description' => 'The book is way better than the movie.',
             'author' => 'Wells, H. G.'
@@ -132,9 +140,12 @@ class BooksControllerTest extends TestCase
     /** @test **/
     public function destroy_should_remove_a_valid_book()
     {
-        $this->delete('/books/1')
+        $books = factory('App\Book')->create();
+        $this->delete("/books/{$books->id}")
             ->seeStatusCode(204)
             ->isEmpty();
+        
+        $this->notSeeInDatabase('books', ['id' => $books->id]);
     }
 
     /** @test **/
